2ed6e0c2aad177c03179551caf778d0535a5b851
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-11-19 12:07:10 -0300

    Add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Factory that creates Player objects
 const Player = function(name, marker) {
     return {
         name,
@@ -5,10 +6,12 @@ const Player = function(name, marker) {
     }
 }
 
+// Game State module to keep information about the state of the game
 const GameState = (function(_player1, _player2) {
     let _curPlayer = _player1;
     let _isFinished = false;
 
+    // Changes the currently playing player
     const changeCurPlayer = () => {
         if(_curPlayer === _player1) {
             _curPlayer = _player2;
@@ -29,6 +32,7 @@ const GameState = (function(_player1, _player2) {
     
 })(Player('Josh', 'X'), Player('Bron', 'O'))
 
+// Game Board module to keep information about the game board
 const GameBoard = (function(tableElement) {
 
     const _board = [
@@ -37,6 +41,7 @@ const GameBoard = (function(tableElement) {
         ['_', '_', '_']
     ]
 
+    // Places the Player marker in the board and in the DOM representation of the board
     const _placeMarker = ({ target: clickedElement }) => {
         const { marker } = GameState.getCurPlayer();
         const [row, col] = clickedElement.dataset.coords.split(' ');
@@ -44,16 +49,19 @@ const GameBoard = (function(tableElement) {
         GameState.changeCurPlayer();
     }
 
+    // Repeats an action, like a loop, but cleaner
     const _repeatAction = (action, times) => {
         for(let i = 0; i < times; i++) {
             action(i);
         }
     }
 
+    // Creates one board row and returns it
     const _makeRow = () => {
         return document.createElement('tr');
     }
 
+    // Creates one board column and returns it
     const _makeCol = (x, y) => {
         const col = document.createElement('td');
         col.dataset.coords = `${x} ${y}`;
@@ -62,6 +70,7 @@ const GameBoard = (function(tableElement) {
         return col;
     }
 
+    // Fills one board row with columns
     const _fillRow = (row, rowNum) => {
         _repeatAction((colNum) => {
             row.append(_makeCol(rowNum, colNum));
@@ -69,8 +78,10 @@ const GameBoard = (function(tableElement) {
         return row;
     }
 
+    // Returns one copy of the boards in its current state
     const getBoard = () => [..._board.map(row => [...row])];
 
+    // Creates all the board elements and renders everything in the DOM
     const initialize = () => {
         _repeatAction((rowNum) => {
             const row = _makeRow();
@@ -86,6 +97,7 @@ const GameBoard = (function(tableElement) {
 
 })(document.querySelector('table'))
 
+// Ties everything together to make the game work
 const Brain = (function() {
     GameBoard.initialize();
 })()
