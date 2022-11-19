ce785f6cda4a3b8543cd7fd668af58c4df6dfbe4
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-11-19 11:59:26 -0300

    Create Game State

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,37 +5,87 @@ const Player = function(name, marker) {
     }
 }
 
+const GameState = (function(_player1, _player2) {
+    let _curPlayer = _player1;
+    let _isFinished = false;
+
+    const changeCurPlayer = () => {
+        if(_curPlayer === _player1) {
+            _curPlayer = _player2;
+        } else {
+            _curPlayer = _player1;
+        }
+    }
+ 
+
+    const getCurPlayer = () => _curPlayer;
+    const isGameFinished = () => _isFinished;
+
+    return {
+        changeCurPlayer,
+        getCurPlayer,
+        isGameFinished
+    }
+    
+})(Player('Josh', 'X'), Player('Bron', 'O'))
+
 const GameBoard = (function(tableElement) {
+
     const _board = [
         ['_', '_', '_'],
         ['_', '_', '_'],
         ['_', '_', '_']
     ]
 
-    const _placeMarker = ({ target: clickedElement }, marker) => {
+    const _placeMarker = ({ target: clickedElement }) => {
+        const { marker } = GameState.getCurPlayer();
         const [row, col] = clickedElement.dataset.coords.split(' ');
-        _board[Number(row)][Number[col]] = marker;
+        _board[Number(row)][Number(col)] = clickedElement.textContent =  marker;
+        GameState.changeCurPlayer();
     }
 
-    const showBoard = () => {
-        for(let i = 0; i < 3; i++) {
-            const row = document.createElement('tr');
-            for(let j = 0; j < 3; j++) {
-                const col = document.createElement('td');
-                col.dataset.coords = `${i} ${j}`;
-                col.addEventListener('click', _placeMarker, { once: true });
-                row.append(col);
-            }
-            tableElement.append(row);
+    const _repeatAction = (action, times) => {
+        for(let i = 0; i < times; i++) {
+            action(i);
         }
     }
 
+    const _makeRow = () => {
+        return document.createElement('tr');
+    }
+
+    const _makeCol = (x, y) => {
+        const col = document.createElement('td');
+        col.dataset.coords = `${x} ${y}`;
+        col.addEventListener('click', _placeMarker, { once: true });
+        col.classList = 'square';
+        return col;
+    }
+
+    const _fillRow = (row, rowNum) => {
+        _repeatAction((colNum) => {
+            row.append(_makeCol(rowNum, colNum));
+        }, 3);
+        return row;
+    }
+
+    const getBoard = () => [..._board.map(row => [...row])];
+
+    const initialize = () => {
+        _repeatAction((rowNum) => {
+            const row = _makeRow();
+            _fillRow(row, rowNum);
+            tableElement.append(row)
+        }, 3)
+    }
+
     return {
-        showBoard
+        initialize,
+        getBoard
     }
 
 })(document.querySelector('table'))
 
 const Brain = (function() {
-    GameBoard.showBoard();
+    GameBoard.initialize();
 })()
