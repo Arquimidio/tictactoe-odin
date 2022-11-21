493ef9ce1f1d24bb80cfef94022e6c3eee1d5419
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-11-21 06:14:18 -0300

    Add starting screen with player naming

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,42 +6,13 @@ const Player = function(name, marker) {
     }
 }
 
-// Game State module to keep information about the state of the game
-const GameState = (function(_player1, _player2) {
-    let _curPlayer = _player1;
-    let _victory = false;
-
-    // Changes the currently playing player
-    const changeCurPlayer = () => {
-        if(_curPlayer === _player1) {
-            _curPlayer = _player2;
-        } else {
-            _curPlayer = _player1;
-        }
-    }
- 
-    const getVictory = () => _victory;
-    const setVictory = (victory) => _victory = victory;
-    const getCurPlayer = () => _curPlayer;
-    const isGameFinished = () => !!getVictory();
-
-    return {
-        changeCurPlayer,
-        getCurPlayer,
-        getVictory,
-        setVictory,
-        isGameFinished
-    }
-    
-})(Player('Josh', 'X'), Player('Bron', 'O'))
-
 // Game Board module to keep information about the game board
 const GameBoard = (function(tableElement) {
 
     const _board = [
-        ['_', '_', '_'],
-        ['_', '_', '_'],
-        ['_', '_', '_']
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
     ]
 
     const rowCoords = [
@@ -61,11 +32,33 @@ const GameBoard = (function(tableElement) {
         [[2, 0], [1, 1], [0, 2]]
     ]
 
+    const _chunk = (arr, size) => {
+        if(arr.length <= size) return [arr];
+        const cur = arr.slice(0, size);
+        const rest = _chunk(arr.slice(size), size);
+        return [cur, ...rest];
+    }
+
+    const getDOMBoard = () => {
+        const squareArr = [...document.querySelectorAll('.square')];
+        return _chunk(squareArr, 3)
+    }
+
+    const _getCoords = (element) => {
+        return element.dataset.coords.split(' ');
+    }
+
     // Places the Player marker in the board and in the DOM representation of the board
-    const placeMarker = ({ target: clickedElement }) => {
-        const { marker } = GameState.getCurPlayer();
-        const [row, col] = clickedElement.dataset.coords.split(' ');
-        _board[Number(row)][Number(col)] = clickedElement.textContent =  marker;
+    const placeMarker = ({ target: clickedElement }, curPlayer) => {
+        const { marker } = curPlayer;
+        const [row, col] = _getCoords(clickedElement);
+        _board[Number(row)][Number(col)] = marker;
+        clickedElement.textContent = marker;
+    }
+
+    const isMarked = ({ target: clickedElement }) => {
+        const [row, col] = _getCoords(clickedElement);
+        return !!_board[row][col];
     }
 
     // Repeats an action, like a loop, but cleaner
@@ -118,7 +111,9 @@ const GameBoard = (function(tableElement) {
         initialize,
         placeMarker,
         getBoard,
+        getDOMBoard,
         highlightSquares,
+        isMarked,
         rowCoords,
         colCoords,
         diagCoords
@@ -127,16 +122,38 @@ const GameBoard = (function(tableElement) {
 })(document.querySelector('table'))
 
 // Ties everything together to make the game work
-const Brain = (function() {
-    GameBoard.initialize();
-    const allSquares = [...document.querySelectorAll('.square')];
+const Brain = (function(_player1, _player2) {
+    const startForm = document.querySelector('.start-form');
 
-    const _chunk = (arr, size) => {
-        if(arr.length <= size) return [arr];
-        const cur = arr.slice(0, size);
-        const rest = _chunk(arr.slice(size), size);
-        return [cur, ...rest];
+    const GameState = {
+        _player1: null,
+        _player2: null,
+        _curPlayer: null,
+        _victory: false
     }
+
+    const startGame = (event) => {
+        event.preventDefault();
+        GameBoard.initialize()
+        const data = [...new FormData(startForm).values()];
+        const [_player1, _player2] = data;
+        GameState._player1 = Player(_player1, 'X');
+        GameState._player2 = Player(_player2, 'O');
+        GameState._curPlayer = _player1;
+        const allSquares = [...document.querySelectorAll('.square')];
+        allSquares.forEach(square => square.addEventListener('click', _play));
+    }
+
+    // Changes the currently playing player
+    const _changeCurPlayer = () => {
+        if(GameState._curPlayer === GameState._player1) {
+            GameState._curPlayer = GameState._player2;
+        } else {
+            GameState._curPlayer = GameState._player1;
+        }
+    }
+ 
+    const _isGameFinished = () => !!GameState._victory;
 
     const _checkSquares = (array, board) => {
         for(let arr of array) {
@@ -157,22 +174,22 @@ const Brain = (function() {
     }
     
     const _end = () => {
-        console.log(GameState.getCurPlayer());
+        console.log(GameState._curPlayer);
     }
 
     const _play = (event) => {
-        if(GameState.isGameFinished()) return;
-        GameBoard.placeMarker(event);
+        if(_isGameFinished() || GameBoard.isMarked(event)) return;
+        GameBoard.placeMarker(event, GameState._curPlayer);
 
         const victoryCoords = _checkVictory();
         if(victoryCoords) {
-            GameState.setVictory(_checkVictory());
-            GameBoard.highlightSquares(_chunk(allSquares, 3), victoryCoords);
+            GameState._victory = victoryCoords;
+            GameBoard.highlightSquares(GameBoard.getDOMBoard(), victoryCoords);
             _end();
         } else {
-            GameState.changeCurPlayer();
+            _changeCurPlayer();
         }
     }
 
-    allSquares.forEach(square => square.addEventListener('click', _play, { once: true }));
+    startForm.addEventListener('submit', startGame);
 })()
